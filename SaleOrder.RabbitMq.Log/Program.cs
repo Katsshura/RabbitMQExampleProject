7a4109f30e5aa0badab9ebe28f6e777709a18ab0
Emerson Rocha Alves <eralves@viajanetsp.local>
2019-08-08 17:52:04 -0300

    Fixed .gitignore to upload Log project

---
+++ SaleOrder.RabbitMq.Log/Program.cs
@@ -0,0 +1,26 @@
+﻿using SaleOrder.RabbitMq.Domain.Configuration.Structs;
+using SaleOrder.RabbitMq.Infra.DI;
+using SaleOrder.RabbitMq.Infra.Rabbit;
+using SimpleInjector;
+using System;
+
+namespace SaleOrder.RabbitMq.Log
+{
+    class Program
+    {
+        static readonly Container container = DIBootStart.Container;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receiving Logs...... \n");
+
+            var receiver = container.GetInstance<RabbitListener>();
+            receiver.Subscribe(QueueType.Log, LogMessage);
+        }
+
+        private static void LogMessage(string message)
+        {
+            Console.WriteLine("Logging: {0}", message);
+        }
+    }
+}
